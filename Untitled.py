98625586da0b381970bc2585db7e53e73bd3bc55
bsrkrishna78 <160612687+bsrkrishna78@users.noreply.github.com>
2025-01-08 12:46:45 +0530

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+m23achine_learning= 1000
+
+
+# In[3]:
+
+
+a=10
+a*=12
+print(a)
+
+
+# In[6]:
+
+
+a=100
+b=200
+print(a+b)
+print(a-b)
+print(b/a)
+
+
+# In[14]:
+
+
+a=input("enter a number")
+b=input("enter a number")
+print(a+b)
+
+
+# In[15]:
+
+
+a=input("enter a string")
+b=input("enter a string")
+print(a+b)
+
+
+# In[16]:
+
+
+a=200
+b=350
+c=400
+print(a>=b and c>a or c>b)
+print(b<c and b>a or c>=a)
+
+
+# In[17]:
+
+
+x=10
+b=not x
+print (b)
+
+
+# In[18]:
+
+
+print(2==2)
+print(2!=2)
+
+
+# In[19]:
+
+
+otp=int(input("enter otp"))
+if otp==2357:
+        print('correct')
+else:
+        print('enter a correct otp')
+    
+
+
+# In[1]:
+
+
+a="machine learning"
+
+
+# In[2]:
+
+
+a[0]
+
+
+# In[3]:
+
+
+a[10]
+
+
+# In[5]:
+
+
+a[0:7]
+
+
+# In[6]:
+
+
+a[-2]
+
+
+# In[7]:
+
+
+a[0:7:1]
+
+
+# In[8]:
+
+
+a[0:7:2]
+
+
+# In[9]:
+
+
+a[0:10:3]
+
+
+# In[12]:
+
+
+a[10:0:-1]
+
+
+# In[13]:
+
+
+a[::-1]
+
+
+# In[17]:
+
+
+a="deep"
+b="learning"
+print(a+b)
+print(a*10)
+
+
+# In[ ]:
+
+
+
+
